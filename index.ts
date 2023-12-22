25841b26f60276ad1430c899a8951c3bf7c4cf59
Jaxaxx <ming.li@fftai.com>
2023-12-22 14:56:19 +0800

    update

--- index.ts
+++ index.ts
@@ -1,11 +1,9 @@
-
-
-import { Human, ArmAction, Motor, HandAction, BodyAction } from "./lib/robot/human";
-import { Car, CarMod } from "./lib/robot/car";
+import {ArmAction, BodyAction, HandAction, Human, Motor} from "./lib/robot/human";
+import {Car, CarMod} from "./lib/robot/car";
 import axios from "axios";
-import { ConnectOption, RobotBase } from "./lib/robot/robot_base";
-import { Camera } from "./lib/common/camera";
-import { System } from "./lib/common/system";
+import {ConnectOption, RobotBase} from "./lib/robot/robot_base";
+import {Camera} from "./lib/common/camera";
+import {System} from "./lib/common/system";
 
 /**
  * This function allows you to query the type of a robot when working with clusters or multiple robots.
@@ -14,7 +12,7 @@ import { System } from "./lib/common/system";
  * @param {ConnectOption} option - Default connection is 127.0.0.1:8001. Modify as needed {host: string, port: number}.
  */
 const get_robot_type = async (option?: ConnectOption) => {
-    const { ssl = false, host = '127.0.0.1', port = '8001' } = option ?? {};
+    const {ssl = false, host = '127.0.0.1', port = '8001'} = option ?? {};
     if (ssl) {
         return axios.get(`https://${host}:${port}/robot/type`);
     } else {
@@ -22,17 +20,32 @@ const get_robot_type = async (option?: ConnectOption) => {
     }
 };
 
-const car = new Car();
-car.set_mode(CarMod.MOD_ACTION)
-    .then(result => {
-        console.log(result);
-    })
-    .catch(error => {
-        console.error(error);
-    });
-
 // Exporting elements using CommonJS syntax
-module.exports = { Human, Car, CarMod, get_robot_type, ArmAction, RobotBase, Motor, HandAction, BodyAction }
+module.exports = {
+    Human,
+    Car,
+    CarMod,
+    Camera,
+    System,
+    get_robot_type,
+    ArmAction,
+    RobotBase,
+    Motor,
+    HandAction,
+    BodyAction
+}
 // Exporting elements using ES6 syntax
-export { Human, Car, CarMod, Camera, System, get_robot_type, ArmAction, RobotBase, Motor, HandAction, BodyAction };
+export {
+    Human,
+    Car,
+    CarMod,
+    Camera,
+    System,
+    get_robot_type,
+    ArmAction,
+    RobotBase,
+    Motor,
+    HandAction,
+    BodyAction
+};
 
--- test/test_robot.ts
+++ test/test_robot.ts
@@ -1,19 +1,19 @@
-import {Car, get_robot_type, Human, CarMod} from '../index'
-import { ArmAction } from '../lib/robot/human';
+import {Car, CarMod, get_robot_type, Human} from '../index'
 
 const sleep = (second: number) => new Promise((resolve) => setTimeout(resolve, second * 1000));
 
+let robot_ip = '192.168.10.101'
 
-get_robot_type({host: '192.168.9.17'}).then((res: any) => {
+get_robot_type({host: robot_ip}).then((res: any) => {
     let type = res.data.data;
 
     if (type === 'human') {
-        let human = new Human({host: '192.168.9.17'});
-        sleep(5).then(() => test_robot(human) )
+        let human = new Human({host: robot_ip});
+        sleep(5).then(() => test_robot(human))
 
     }
     if (type === 'car') {
-        let car = new Car({host: '192.168.9.17'});
+        let car = new Car({host: robot_ip});
         sleep(5).then(() => test_car(car))
 
     }
@@ -21,13 +21,14 @@ get_robot_type({host: '192.168.9.17'}).then((res: any) => {
 
 
 const test_robot = async (human: Human) => {
+    await human.walk(0, 0)
     // await human.start();
     // await sleep(10)
     // await human.stand()
     // await sleep(5)
     // await human.enable_debug_state(1)
     // human.walk(0.1, 0.1)
-    await human.upper_body(ArmAction.ARMS_SWING)
+    // await human.upper_body(ArmAction.ARMS_SWING)
 }
 
 const test_car = async (car: Car) => {
